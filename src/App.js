74eada18666f228b22103d6f59b98ef0ea59cda6
Himanshudevg2000 <himanshudevgade87@gmail.com>
2023-03-16 23:30:20 +0530

    Added Restraunt Summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Header/Header";
+import RestrauntSummary from "./Components/RestrauntSummary/RestrauntSummary";
 
 function App() {
   return (
     <div>
       <Header />
+      <RestrauntSummary />
     </div>
   );
 }
